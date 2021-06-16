924be166e154c6d98f34683e05d49b225edb1c02
kjs0619-prog <84962654+kjs0619-prog@users.noreply.github.com>
2021-06-16 16:03:56 +0900

    오늘도 자바파일 하나 커밋했어요. 자바와 사랑에 빠졌어요.

---
+++ myjavaproject2/src/myjavaproject2/mypackage/MyJavaProject.java
@@ -0,0 +1,13 @@
+package myjavaproject2.mypackage;
+
+
+public class MyJavaProject {
+
+	public static void main(String[] args) {
+
+//		System.out.println("얀녕하세요");
+//		System.out.println("얀녕하세요2");
+		System.out.println("제이름은 000입니");
+	}
+
+}
