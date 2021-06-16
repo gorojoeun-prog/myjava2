924e82630690f77fb2b04a583db8fa766a5c992e
kjs0619-prog <84962654+kjs0619-prog@users.noreply.github.com>
2021-06-16 16:57:20 +0900

    메시지

--- myjavaproject2/src/myjavaproject2/mypackage/MyJavaProject.java
+++ myjavaproject2/src/myjavaproject2/mypackage/MyJavaProject.java
@@ -8,6 +8,10 @@ public class MyJavaProject {
 //		System.out.println("얀녕하세요");
 //		System.out.println("얀녕하세요2");
 		System.out.println("제이름은 000입니");
+		int a = 3;
+
+		System.out.println("a의 값은"+a+"입니다.");
+		
 	}
 
 }
